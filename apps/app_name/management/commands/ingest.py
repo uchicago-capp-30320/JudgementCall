9b01ec198718094e326d25420c397359c86b012d
Maggie Larson <maggiel4rson@gmail.com>
2026-04-16 16:12:20 -0500

    ingestion functions for court data from courtlistener and archived NCSC page

---
+++ apps/app_name/management/commands/ingest.py
@@ -0,0 +1,20 @@
+import ingestion
+from apps.app_name.models import (
+    Court
+)
+
+"""
+TO DO: decide how to format this - one ingest function to be called from the command line for each dataset?
+courts will probably need to be created once, 
+people and tenure will need to be updated occasionally,
+cases will need to be updated regularly
+"""
+
+def create_courts():
+    courts_data = ingestion.ingest_courts_data()
+    for court in courts_data:
+        continue # don't want to touch database yet!
+        Court.objects.update_or_create(
+            name=court["name"],
+            #etc
+        )
---
+++ ingestion/ingest_courts_data.py
@@ -0,0 +1,83 @@
+"""
+One-off ingestion functions to create court records.
+Currently return pandas dataframes - TBD on final output!
+
+Sources: 
+- CourtListener API - https://www.courtlistener.com/api/rest/v4/courts/
+- Web Archive NCSC Judicial Selection - http://web.archive.org/web/20151022190427/http://www.judicialselection.us/judicial_selection/methods/selection_of_judges.cfm?state=
+
+"""
+import sys
+import pandas as pd
+import requests
+import lxml.html
+from collections import defaultdict
+
+ARCHIVE_NCSC_URL = "http://web.archive.org/web/20151022190427/http://www.judicialselection.us/judicial_selection/methods/selection_of_judges.cfm?state="
+
+CL_BASE_URL = "https://www.courtlistener.com/"
+CL_COURTS = "api/rest/v4/courts/"
+API_TOKEN = "REPLACE WITH YOUR API TOKEN"
+
+def scrape_ncsc_archive():
+    r = requests.get(ARCHIVE_NCSC_URL)
+    response = lxml.html.fromstring(r.text)
+
+    # state data dict format: {"State_Name": {"Court_Name": {court data dict}}, "State_Name": {},...}
+    state_data_dict = defaultdict(lambda: defaultdict(dict))
+
+    for item in response.xpath('//div[@id="content"]/*'):
+        if item.tag in ['h2', 'h3', 'p']:
+            continue
+        # update the state
+        elif item.tag == 'div' and 'yellow_box' in item.classes:
+            state_str = item.xpath('h4/text()')[0]
+        
+        # get info for state
+        elif item.tag == 'table':
+            state_data_str = item.text_content()
+            state_data_list = [item for item in state_data_str.replace('\t', '').split('\n') if item != '']
+            info_key = state_data_list[0]
+
+            i = 1
+            while i < len(state_data_list) - 1:
+                court = state_data_list[i]
+                info_value = state_data_list[i+1]
+                state_data_dict[state_str][court][info_key] = info_value
+                i += 2      
+    
+    state_data_flat_list = []
+    for state, court_dict in state_data_dict.items():
+        for court, info in court_dict.items():
+            flat = {"State": state, "Court": court, **info}
+            state_data_flat_list.append(flat)
+    df = pd.DataFrame(state_data_flat_list)
+
+    return df
+
+
+def ingest_courtlistener():
+    courts_results = []
+    page_url = CL_BASE_URL + CL_COURTS
+    while page_url is not None:
+        r = requests.get(page_url, headers={"Authorization": f"Token {API_TOKEN}"})
+        print(page_url, r.status_code)
+        response = r.json()
+        results = response["results"]
+        courts_results.extend(results)
+        page_url = response["next"]
+    
+    df = pd.DataFrame(courts_results)
+    return df
+
+
+def main():
+    args = sys.argv[1:]
+    if "ncsc" in args:
+        ncsc_df = scrape_ncsc_archive()
+        print(ncsc_df.head())
+
+    print("Placeholder: this is the main function for ingest_courts_data.py.")
+
+if __name__=="__main__":
+    main()
