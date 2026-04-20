0eda806048eab97b54eb5ceb5197d97e1d9e95db
clleone <clleone@uchicago>
2026-04-20 11:21:53 -0500

    put a basic placeholder in views.py

--- apps/judgement-call/views.py
+++ apps/judgement-call/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
+from django.core.paginator import Paginator
+from .models import DataSet, Publisher, Region, DataSetFile
+from urllib.parse import urlparse
 
-# Create your views here.
+
+def landing(request):
+    """Landing page for Judgement Call users."""
+    context = {
+        "msg": "Welcome to Judgement Call!",
+    }
+
+    return render(request, "home.html", context)
