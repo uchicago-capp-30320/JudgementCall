7a9a6f481c30cda76719f0cccab8d1d818afd526
clleone <clleone@uchicago>
2026-04-24 01:20:33 -0500

    commit needed to pull?

--- apps/judgement_call/apps.py
+++ apps/judgement_call/apps.py
@@ -3,4 +3,8 @@ from django.apps import AppConfig
 
 class AppNameConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
+<<<<<<< HEAD
     name = "apps.judgement_call"
+=======
+    name = "judgement_call"
+>>>>>>> 37e0d64 (commit needed to pull?)
