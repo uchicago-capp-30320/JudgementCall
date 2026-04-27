155fa144be99d3405c3dc6d941090b8deab22575
clleone <clleone@uchicago>
2026-04-26 21:10:36 -0500

    Revert "commit needed to pull?"
    This reverts commit 7a9a6f481c30cda76719f0cccab8d1d818afd526.

--- apps/judgement_call/views.py
+++ apps/judgement_call/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import (
@@ -180,19 +179,3 @@ def add_fake_data(request):
             )
 
     return HttpResponse("Done!")
-=======
-from django.shortcuts import render, get_object_or_404, redirect
-from django.db.models import Q
-from django.core.paginator import Paginator
-from .models import DataSet, Publisher, Region, DataSetFile
-from urllib.parse import urlparse
-
-
-def landing(request):
-    """Landing page for Judgement Call users."""
-    context = {
-        "msg": "Welcome to Judgement Call!",
-    }
-
-    return render(request, "home.html", context)
->>>>>>> 37e0d64 (commit needed to pull?)
