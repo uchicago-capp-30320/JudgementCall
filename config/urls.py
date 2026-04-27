53e1c92ad25f76cc28713d1e36c17f2b27eb6ccb
clleone <clleone@uchicago>
2026-04-26 20:43:22 -0500

    trying to get urls sorted out, disregard

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,10 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-# from apps.judgement_call import views as judge_views
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("djadmin/", admin.site.urls),
-    # path("", judge_views.index),
 ]
 
 if settings.DEBUG and not settings.IS_TESTING:
