00208a103b1c09141c18b8a05d47140ba96403cb
Maggie Larson <maggiel4rson@gmail.com>
2026-04-28 00:21:13 -0500

    first pass at ingestion management command

--- apps/judgement_call/management/commands/ingest.py
+++ apps/judgement_call/management/commands/ingest.py
@@ -1,5 +1,8 @@
-import ingestion
+# import ingestion.ingest_courts_data
+# import ingestion.merge_courts_data
+import csv
 from apps.judgement_call.models import Court
+from django_typer.management import Typer
 
 """
 TO DO: decide how to format this - one ingest function to be called
@@ -9,12 +12,33 @@ people and tenure will need to be updated occasionally,
 cases will need to be updated regularly
 """
 
+app = Typer()
 
-def create_courts():
-    courts_data = ingestion.ingest_courts_data()
-    for court in courts_data:
-        continue  # don't want to touch database yet!
-        Court.objects.update_or_create(
-            name=court["name"],
-            # etc
-        )
+
+@app.command()
+def command(self, data: str):
+    print("Hello world")
+
+    if data == "courts":
+        with open("ingestion/merged_courts.csv", encoding="utf-8") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                headers = row
+                break
+            for row in reader:
+                court = dict(zip(headers, row))
+                court["term_length"] = (
+                    court["term_length"]
+                    .replace(" yrs", "")
+                    .replace("yrs", "")
+                    .replace(" years", "")
+                )
+                try:
+                    court["term_length"] = int(court["term_length"])
+                except ValueError:
+                    court["term_length"] = None
+                try:
+                    court["bench_size"] = int(court["bench_size"])
+                except ValueError:
+                    court["bench_size"] = None
+                Court.objects.update_or_create(**court)
