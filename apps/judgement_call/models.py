5c842dc02183fe003ce5ab4431a5ca4e8637ac40
Ldepablo11 <ldepablo1@uchicago.edu>
2026-05-09 15:51:07 -0500

    Implementing election date inference - 1st pass

--- apps/judgement_call/models.py
+++ apps/judgement_call/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from datetime import date
 from localflavor.us.models import USStateField
+import pandas as pd
 
 
 # drop down types
@@ -127,22 +129,6 @@ class Person(models.Model):
         return self.name_canonical
 
 
-class Election(models.Model):
-    court = models.ForeignKey(Court, on_delete=models.PROTECT)
-    date = models.DateField()
-
-    def __str__(self):
-        return f"{self.date} election for {self.court}"
-
-
-class Candidacy(models.Model):
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
-    election = models.ForeignKey(Election, on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name_plural = "Candidacies"
-
-
 class Tenure(models.Model):
     court = models.ForeignKey(Court, on_delete=models.PROTECT)
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
@@ -156,6 +142,34 @@ class Tenure(models.Model):
 
     def __str__(self):
         return f"{self.person} - {self.court}"
+
+
+class Election(models.Model):
+    court = models.ForeignKey(Court, on_delete=models.PROTECT)
+    election_date = models.DateField()
+    incumbent = models.ForeignKey(Tenure, on_delete=models.PROTECT)
+
+    def deduce_elections(self):
+        court_elections = Tenure.objects.values("id", "court_id", "end_date")
+        election_df = pd.DataFrame(list(court_elections))
+
+        for index, row in election_df.iterrows():
+            tenure = Tenure.objects.get(pk=row["id"])
+            court = Court.objects.get(pk=row["court_id"])
+            term_end = row["end_date"]
+            elect_date = term_end.replace(year=term_end.year - 1, month=11, day=5)
+            Election.objects.create(court=court, election_date=elect_date, incumbent=tenure)
+
+    def __str__(self):
+        return f"{self.date} election for {self.court}"
+
+
+class Candidacy(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    election = models.ForeignKey(Election, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Candidacies"
 
 
 class Alias(models.Model):
