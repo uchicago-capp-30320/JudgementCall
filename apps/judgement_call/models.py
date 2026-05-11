003060cb59956d987afb1d3e609221cb8e052d90
hnmns <rileykouns@gmail.com>
2026-05-11 11:00:38 -0500

    Update apps/judgement_call/models.py

--- apps/judgement_call/models.py
+++ apps/judgement_call/models.py
@@ -199,7 +199,7 @@ class Alias(models.Model):
             self.tenure = top_match
             self.save()
         else:
-            try_alias = self.alias.replace("justice", "").replace(" j ", " ")
+            try_alias = self.alias.replace("justice", "").replace(" j ", " ").replace("senior", "").replace("chief", "")
             if try_alias != self.alias:
                 print(f"rerunning with {try_alias}")
                 matches = self.find_matches(try_alias)
