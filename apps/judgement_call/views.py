fd4ebd7b0f442a0f7f8a43dad052567b3761a919
clleone <clleone@uchicago>
2026-05-13 10:40:38 -0500

    fixed elections bugs related to 'date' turning into 'election_date'

--- apps/judgement_call/views.py
+++ apps/judgement_call/views.py
@@ -61,7 +61,32 @@ def get_counties(request, state):
     return JsonResponse(list(counties), safe=False)
 
 
+def judge_sort(judge_lst):
+    ordered_lst = []
+
+    for j in judge_lst:
+        if j["chief_justice"]:
+            chief_j = j
+        else:
+            if len(ordered_lst) == 0:
+                ordered_lst.append(j)
+            for i, sj in enumerate(ordered_lst):
+                if sj["name"] > j["name"]:
+                    ordered_lst.insert((i - 1), j)
+
+    return [chief_j] + ordered_lst
+
+
 def judges_state_county(request, state, county):
+    """
+    courts structure --
+    courts {
+        court_name: {
+            upcoming_election: T/F
+            judges: [judge1_dict, judge2_dict, judge3_dict...]
+        }
+    }
+    """
     # grab all the tenures associated with a specific state / county
     geo_c2c = CountyToCourt.objects.filter(state=state, county=county)
     local_courts_list = Court.objects.filter(countytocourt__in=geo_c2c)
@@ -84,7 +109,7 @@ def judges_state_county(request, state, county):
         # a value in the {court: [tenure_info, tenure_info]} type dict
         courts[court_name]["judges"].append(
             {
-                "name": tenure.person.name_canonical,
+                "name": tenure.person.name_canonical.title(),
                 "chief_justice": tenure.chief_justice,
                 "party_registration": tenure.person.party_registration.title(),
                 "more_info": f"/people/{tenure.person.id}/",
@@ -92,6 +117,10 @@ def judges_state_county(request, state, county):
                 "end_date": tenure.end_date,
             }
         )
+
+    # for c in courts.keys():
+    #     c["judges"] = judge_sort(c["judges"])
+
     context = {
         "courts": courts,
         "state": state,
@@ -105,13 +134,13 @@ def show_person(request, person_id):
     tenures = Tenure.objects.filter(person=person)
 
     person_info = {
-        "name": person.name_canonical,
+        "name": person.name_canonical.title(),
         "birth_date": person.birth_date,
-        "gender": person.gender,
-        "race": person.race,
+        "gender": person.gender.title(),
+        "race": person.race.title(),
         "party_registration": person.party_registration.title(),
-        "professional_experience": person.professional_experience,
-        "law_school": person.law_school,
+        "professional_experience": person.professional_experience.title(),
+        "law_school": person.law_school.title(),
     }
 
     person_tenures = []
@@ -121,10 +150,10 @@ def show_person(request, person_id):
                 "court": tenure.court.name,
                 "start_date": tenure.start_date,
                 "end_date": tenure.end_date,
-                "selection_type": tenure.selection_type,
-                "ticket_party": tenure.ticket_party,
-                "appointer_name": tenure.appointer_name,
-                "appointer_party": tenure.appointer_party,
+                "selection_type": tenure.selection_type.title(),
+                "ticket_party": tenure.ticket_party.title(),
+                "appointer_name": tenure.appointer_name.title(),
+                "appointer_party": tenure.appointer_party.title(),
                 "chief_justice": tenure.chief_justice,
             }
         )
@@ -190,14 +219,14 @@ def get_upcoming_elections(relevant_courts):
     Takes in QSet of courts and returns those with nearest associated election date
     """
     next_event = (
-        Election.objects.filter(date__gte=datetime.now())
-        .order_by("date")
-        .values_list("date", flat=True)
+        Election.objects.filter(election_date__gte=datetime.now())
+        .order_by("election_date")
+        .values_list("election_date", flat=True)
         .first()
     )
 
     if next_event:
-        return Election.objects.filter(court__in=relevant_courts, date=next_event)
+        return Election.objects.filter(court__in=relevant_courts, election_date=next_event)
 
     return Election.objects.none()
 
@@ -225,7 +254,10 @@ def elections_state_county(request, state, county):
     # want to retrieve soonest elections
     local_elections_list = get_upcoming_elections(local_courts_list)
     elections = {
-        e.court.name: {"date": e.date.strftime("%m-%d-%Y"), "type": e.court.selection_method}
+        e.court.name: {
+            "date": e.election_date.strftime("%m-%d-%Y"),
+            "type": e.court.selection_method.title(),
+        }
         for e in local_elections_list
     }
 
