c467360d3b623cbc9276119f7b9a71bbd581d15b
Ldepablo11 <ldepablo1@uchicago.edu>
2026-05-22 12:41:45 -0500

    Making it a command

--- apps/judgement_call/management/commands/scrape_cases.py
+++ apps/judgement_call/management/commands/scrape_cases.py
@@ -2,9 +2,9 @@ import csv
 from pathlib import Path
 import string
 import us
+from django.core.management.base import BaseCommand
 from django_typer.management import Typer
-from ingestion.setup_us_states_counties import make_county_to_court_df
-from ingestion.ingest_cases_opinions import produce_tables, generate_case_id
+from ingestion.ingest_cases_opinions import produce_tables
 from ingestion.ingest_sc_cases import scrape_scdb
 from ingestion.ingest_courts import (
     MERGED_COURTS_PATH,
@@ -38,101 +38,39 @@ cases will need to be updated regularly
 app = Typer()
 
 
-@app.command()
-def command(self):
-    # TODO: scrape State Case Database
-    all_current_cases = scrape_scdb(write_on=False)
+class Command(BaseCommand):
+    def ingest_new_cases():
+        # TODO: scrape State Case Database
+        all_current_cases = scrape_scdb(write_on=False)
 
-    # check if any are new
-    # eg new_cases = df[df["docket_id"].notin(all_cases)]
-    db_cases_ids = [case["case_id"] for case in Case.objects.values("case_id")]
-    new_cases = all_current_cases[~all_current_cases["case_id"].isin(db_cases_ids)]
-    print("New cases not in database:")
-    print(new_cases)
+        # check if any are new
+        # eg new_cases = df[df["docket_id"].notin(all_cases)]
+        db_cases_ids = [case["case_id"] for case in Case.objects.values("case_id")]
+        new_cases = all_current_cases[~all_current_cases["case_id"].isin(db_cases_ids)]
+        print("New cases not in database:")
+        print(new_cases)
 
-    # run LLM processing on new_cases if there are any
-    if not new_cases.empty:
-        prompt_path = Path(__file__).parent.parent.parent.parent.parent / "ingestion" / "prompt.txt"
-        table_dic, run_metadata = produce_tables(new_cases, prompt_path, use_existing=False)
+        # run LLM processing on new_cases if there are any
+        if not new_cases.empty:
+            prompt_path = (
+                Path(__file__).parent.parent.parent.parent.parent / "ingestion" / "prompt.txt"
+            )
+            table_dic, run_metadata = produce_tables(new_cases, prompt_path, use_existing=False)
 
-        # Insert cases into database
-        cases = table_dic["case_table"].reset_index(drop=True).to_dict(orient="records")
-        for case in cases:
-            Case.objects.update_or_create(**case)
+            # Insert cases into database
+            cases = table_dic["case_table"].reset_index(drop=True).to_dict(orient="records")
+            for case in cases:
+                Case.objects.update_or_create(**case)
 
-        # Insert individual opinions into database
-        ind_opinions = (
-            table_dic["individual_opinion_table"].reset_index(drop=True).to_dict(orient="records")
-        )
-        for ind_op in ind_opinions:
-            IndividualOpinion.objects.update_or_create(**ind_op)
+            # Insert individual opinions into database
+            ind_opinions = (
+                table_dic["individual_opinion_table"]
+                .reset_index(drop=True)
+                .to_dict(orient="records")
+            )
+            for ind_op in ind_opinions:
+                IndividualOpinion.objects.update_or_create(**ind_op)
 
-        # Insert run metadata into database
-        run_metadata.pop("cases_processed")
-        CaseProcessingRun.objects.update_or_create(**run_metadata)
-
-
-# HELPER FUNCTIONS
-
-
-def empty_string_to_none(value):
-    return value if value != "" else None
-
-
-def standardize_alias(alias: str):
-    return alias.strip().lower().translate(str.maketrans("", "", string.punctuation))
-
-
-case_csv_cols = [
-    "",
-    "case_id",
-    "docket_no",
-    "title",
-    "state",
-    "date",
-    "type",
-    "description",
-    "plaintiff_argument",
-    "defendant_argument",
-    "decision_outcome",
-    "decision_winner",
-    "environment",
-    "consumers",
-    "reproductive_rights",
-    "democratic_norms",
-    "free_press",
-    "public_health",
-    "separation_church_state",
-    "voting_access",
-    "public_education",
-    "free_speech",
-    "privacy",
-    "worker_rights",
-]
-
-case_model_cols = [
-    "",
-    "case_id",
-    "docket_no",
-    "case_title",
-    "state",
-    "decision_date",
-    "case_type",
-    "description",
-    "plaintiff_argument",
-    "defendant_argument",
-    "decision_outcome",
-    "decision_winner",
-    "environment",
-    "consumers",
-    "reproductive_rights",
-    "democratic_norms",
-    "free_press",
-    "public_health",
-    "separation_church_state",
-    "voting_access",
-    "public_education",
-    "free_speech",
-    "privacy",
-    "worker_rights",
-]
+            # Insert run metadata into database
+            run_metadata.pop("cases_processed")
+            CaseProcessingRun.objects.update_or_create(**run_metadata)
