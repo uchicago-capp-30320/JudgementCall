d2fd0e002fd005501c894a3d773a6f89ad337ba5
clleone <clleone@uchicago>
2026-05-25 20:34:51 -0500

    fixed start_date/end_date null bug for MA

--- apps/judgement_call/icons.py
+++ apps/judgement_call/icons.py
@@ -57,21 +57,35 @@ TOPIC_REPHRASED = {
 # up for election soon
 def up_for_election(tenure):
     "returns bool"
-    return (
-        tenure.tenure_length_remaining <= 1,
-        "#808080ff",
-        "This judge's term ends soon.",
-    )
+    if tenure.end_date is not None:
+        return (
+            tenure.tenure_length_remaining <= 1,
+            "#808080ff",
+            "This judge's term ends soon.",
+        )
+    else:
+        return (
+            False,
+            "#808080ff",
+            "This judge's term ends soon.",
+        )
 
 
 # long tenure (>=10yr)
 def long_tenure(tenure):
     "returns bool indicating >=10yr of current tenure"
-    return (
-        tenure.tenure_length_to_date >= 10,
-        "#808080ff",
-        "This judge has served for 10+ years.",
-    )
+    if tenure.start_date is not None:
+        return (
+            tenure.tenure_length_to_date >= 10,
+            "#808080ff",
+            "This judge has served for 10+ years.",
+        )
+    else:
+        return (
+            False,
+            "#808080ff",
+            "This judge has served for 10+ years.",
+        )
 
 
 def effective_stance(alignment, indiv_ruling):
@@ -189,14 +203,15 @@ def has_scandals():
 
 def get_judge_icons(tenure, icon_dict):
     "get icons related to judges"
-    icon_dict["release_alert"] = up_for_election(tenure)
-    icon_dict["hourglass"] = long_tenure(tenure)
-    topics_to_include = topics_of_note(tenure)
-    topic_icons = {
-        TOPIC_ICON_DICT[k]: v for k, v in topics_to_include.items() if k in TOPIC_ICON_DICT
-    }
+    if tenure is not None:
+        icon_dict["release_alert"] = up_for_election(tenure)
+        icon_dict["hourglass"] = long_tenure(tenure)
+        topics_to_include = topics_of_note(tenure)
+        topic_icons = {
+            TOPIC_ICON_DICT[k]: v for k, v in topics_to_include.items() if k in TOPIC_ICON_DICT
+        }
 
-    icon_dict = icon_dict | topic_icons
+        icon_dict = icon_dict | topic_icons
 
     return icon_dict
 
